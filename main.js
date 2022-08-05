1fb16d9d11f8380c911ef679021f5bc0c0ab000d
Arka347 <82458894+Arka347@users.noreply.github.com>
2022-08-05 19:33:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video = "";
+statuus = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("statuus").innerHTML = "Status : detecting objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    statuus = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
